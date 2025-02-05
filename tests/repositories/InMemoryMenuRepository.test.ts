7a308e245572c57b9ccf50960df59407713839f5
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2025-02-05 18:17:24 +0200

    add jest test inMemoryRepository

---
+++ tests/repositories/InMemoryMenuRepository.test.ts
@@ -0,0 +1,37 @@
+import { InMemoryMenuRepository } from "../../src/repositories/inMemory/InMemoryMenuRepository";
+import { MenuItem } from "../../src/models/MenuItem";
+
+describe('InMemoryMenuRepository', () => {
+    let repository: InMemoryMenuRepository;
+
+    beforeEach(() => {
+        repository = new InMemoryMenuRepository();
+    })
+
+    test("should return an empty list initially", async () => {
+        const menu = await repository.getAll();
+        expect(menu).toEqual([]);
+    })
+
+    test("should add a new item to the menu", async () => {
+        const item: MenuItem = { id: "1", name: "Gazpacho", price: 14 }
+        await repository.create(item)
+
+        const menu = await repository.getAll();
+        expect(menu).toHaveLength(1);
+        expect(menu[0]).toEqual(item);
+    })
+
+    test("must find element by id", async () => {
+        const item: MenuItem = { id: "1", name: "Burger", price: 9 }
+        await repository.create(item)
+
+        const foundItem = await repository.getById("1");
+        expect(foundItem).toEqual(item);
+    })
+
+    test("should return null if the element is not found", async () => {
+        const foundItem = await repository.getById("938");
+        expect(foundItem).toBeNull()
+    })
+})
---
+++ tests/repositories/InMemoryOrderRepository.test.ts
@@ -0,0 +1,43 @@
+import { InMemoryOrderRepository } from "../../src/repositories/inMemory/InMemoryOrderRepository";
+import { MenuItem } from './../../src/models/MenuItem';
+import { Order } from "../../src/models/Orders";
+
+describe("InMemoryOrderRepository", () => {
+    let repository: InMemoryOrderRepository;
+
+    beforeEach(() => {
+        repository = new InMemoryOrderRepository();
+    })
+
+    test("should return an empty list of orders initially", async () => { 
+        const orders = await repository.getAll();
+        expect(orders).toEqual([]);
+     })
+    
+    test("must add a new order", async () => {
+        const items: MenuItem[] = [
+            { id: "1", name: "Pizza", price: 10 },
+            { id: "2", name: "Cola", price: 2 }
+        ];
+        const order: Order = { id: '1', items, totalPrice: 12, status: "pending" };
+
+        await repository.create(order);
+
+        const orders = await repository.getAll();
+        expect(orders).toHaveLength(1);
+        expect(orders[0]).toEqual(order);
+    });
+    
+    test("should find the order by id", async () => { 
+        const order: Order = { id: "1", items: [], totalPrice: 0, status: "pending" }
+        await repository.create(order);
+        
+        const foundOrder = await repository.getById("1");
+        expect(foundOrder).toEqual(order);
+     })
+    
+    test("should return null if the order is not found", async () => {
+        const foundOrder = await repository.getById("984");
+        expect(foundOrder).toBeNull();
+    })
+})
